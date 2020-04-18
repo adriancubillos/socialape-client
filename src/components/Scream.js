115d52245a17d7f5ca2ff9f6c650b9f4cbd553ff
Adrian <adriancubillos@gmail.com>
2020-04-18 17:58:16 -0400

    Login form and dayjs to display from now dates

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { withStyles, Typography, Card, CardMedia, CardContent } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+
 const styles = {
   card: {
     position: 'relative',
@@ -19,6 +22,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount },
@@ -31,7 +35,7 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant="body2" color="textSecondary">
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant="body1">{body}</Typography>
         </CardContent>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,142 @@
 import React, { Component } from 'react';
+import { withStyles, Grid, Typography, TextField, Button, CircularProgress } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import axios from 'axios';
+import AppIcon from '../images/icon.png';
+import { Link } from 'react-router-dom';
+
+const styles = {
+  form: {
+    textAlign: 'center',
+  },
+  image: {
+    width: 80,
+    margin: '20px auto',
+  },
+  pageTitle: {
+    margin: '10px auto',
+  },
+  textField: {
+    margin: '10px auto',
+  },
+  button: {
+    marginTop: 20,
+    position: 'relative',
+  },
+  customError: {
+    color: 'red',
+    fontSize: '0.8rem',
+    marginTop: 10,
+  },
+  progress: {
+    position: 'absolute',
+  },
+};
 
 class login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      loading: false,
+      errors: {},
+    };
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      loading: true,
+    });
+
+    const userData = {
+      email: this.state.email,
+      password: this.state.password,
+    };
+
+    axios
+      .post('/login', userData)
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          loading: false,
+        });
+        this.props.history.push('/');
+      })
+      .catch((err) => {
+        this.setState({
+          errors: err.response.data,
+          loading: false,
+        });
+      });
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
   render() {
+    const { classes } = this.props;
+    const { errors, loading } = this.state;
     return (
-      <div>
-        <h1>Login page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm />
+        <Grid item sm>
+          <img src={AppIcon} className={classes.image} alt="ape" />
+          <Typography variant="h2" className={classes.pageTitle}>
+            Login
+          </Typography>
+          <form noValidate onSubmit={this.handleSubmit}>
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Email"
+              className={classes.textField}
+              helperText={errors.email}
+              error={errors.email ? true : false}
+              value={this.state.email}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            <TextField
+              id="password"
+              name="password"
+              type="password"
+              label="Password"
+              className={classes.textField}
+              helperText={errors.password}
+              error={errors.password ? true : false}
+              value={this.state.password}
+              onChange={this.handleChange}
+              fullWidth
+            ></TextField>
+            {errors.general && (
+              <Typography variant="body2" className={classes.customError}>
+                {errors.general}
+              </Typography>
+            )}
+            <Button type="submit" disabled={loading} variant="contained" color="primary" className={classes.button}>
+              Login
+              {loading && <CircularProgress size={30} className={classes.progress} />}
+            </Button>
+            <br />
+            <small>
+              If You don't have an account, please sign up <Link to="/signup">here</Link>{' '}
+            </small>
+          </form>
+        </Grid>
+        <Grid item sm />
+      </Grid>
     );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(login);
